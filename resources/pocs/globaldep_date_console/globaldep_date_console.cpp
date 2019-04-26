e10fe563dea48a3e4f9090c2fed3a0eadbcc2918
madt1m <pietrotirenna.pt@gmail.com>
2019-04-26 14:55:11 +0200

    resources: Updated wrathrage and added some pocs

---
+++ resources/pocs/globaldep_date_console/globaldep_date_console.cpp
@@ -0,0 +1,45 @@
+﻿/**
+ * Il secondo POC introduce una problematica un po' più ostica da trattare.
+ * Cosa succede se inseriamo una dipendenza globale ad un valore che viene concretizzato
+ * PRIMA del blocco che contiene la chiamata ad una Trigger source?
+ *
+ * In realtà, angr concretizza già i valori globali nei segmenti di data quando
+ * il progetto viene creato, quindi si riesce a gestire questa situazione.
+ */
+
+#include "stdafx.h"
+
+#include <windows.h>
+#include <stdio.h>
+
+int checktime(void);
+void executePayload(void);
+
+int dayOfTheEnd = 21;
+
+int main()
+{
+
+	if (checktime())
+	{
+		executePayload();
+	}
+}
+
+void executePayload()
+{
+	printf("You got to the payload!\n");
+}
+
+int checktime()
+{
+	SYSTEMTIME x;
+	GetSystemTime(&x);
+	printf("Current day is %d\n", x.wDay);
+
+	if ((x.wDay == dayOfTheEnd))
+	{
+		return 1;
+	}
+	return 0;
+}
---
+++ resources/pocs/localdep_date_console/localdep_date_console.cpp
@@ -0,0 +1,51 @@
+﻿/**
+ * Il problema iniettato in questo POC tocca in pieno le difficoltà della symbolic execution.
+ * Come gestire casi in cui il valore sia stato iniettato e concretizzato PRIMA dello starting point
+ * dell'esecuzione simbolica?
+ */
+
+#include "stdafx.h"
+
+#include <windows.h>
+#include <stdio.h>
+
+int checktime(int *ptr, int size);
+void executePayload(void);
+
+int main()
+{
+	int interesting_days[] = {11, 26, 31};
+
+	if (checktime(interesting_days, 3))
+	{
+		executePayload();
+	}
+}
+
+void executePayload()
+{
+	printf("You got to the payload!\n");
+}
+
+int checktime(int *ptr, int size)
+{
+	char FOUND = false;
+
+	SYSTEMTIME x;
+	GetSystemTime(&x);
+	printf("Current day is %d\n", x.wDay);
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (ptr[i] == x.wDay)
+		{
+			FOUND = true;
+		}
+	}
+
+	if (FOUND)
+	{
+		return 1;
+	}
+	return 0;
+}
